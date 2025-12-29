5b693831343fdf161b2157c3a55e8fcce3e79cc3
Ken <josephken020605@gmail.com>
2025-12-30 00:02:54 +0700

    chore: refactor router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout";
-import ProfileProtectedRoute from "./components/auth/ProfileProtectedRoute";
-import AccountProtectedRoute from "./components/auth/AccountProtectedRoute";
+// import ProfileProtectedRoute from "./components/auth/ProfileProtectedRoute";
+// import AccountProtectedRoute from "./components/auth/AccountProtectedRoute";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -17,89 +16,84 @@ import TestDispatcher from "./components/TestDispatcher";
 import VisualTestLayout from "./pages/VisualTestLayout";
 import LanguageTestLayout from "./pages/LanguageTestLayout";
 import SelectProfile from "./pages/SelectProfile";
-import MiniGame2Layout from "./pages/Minigame2Layout.tsx"; 
+import MiniGame2Layout from "./pages/Minigame2Layout.tsx";
 
 function App() {
-    return (
-        <Router>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        {/* Public Routes */}
-                        <Route index element={<Home />} />
-                        <Route path="about" element={<About />} />
-                        <Route path="login" element={<Login />} />
-                        <Route path="register" element={<Register />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* Public Routes */}
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
 
-                        {/* 👉 Cho chạy thẳng không cần đăng nhập: */}
-                        <Route
-                            path="profile/select"
-                            element={<SelectProfile />}
-                        />
+          {/* 👉 Cho chạy thẳng không cần đăng nhập: */}
+          <Route path="profile/select" element={<SelectProfile />} />
 
-                        {/* Protected Routes (Sử dụng tạm thời cho chạy thẳng) */}
-                        <Route path="me" element={<Profile />} />
-                        <Route path="dashboard" element={<Dashboard />} />
-                        <Route path="human" element={<HumanFeaturesForm />} />
-                        <Route path="test/auditory" element={<AuditoryTest />} />
+          {/* Protected Routes (Sử dụng tạm thời cho chạy thẳng) */}
+          <Route path="me" element={<Profile />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="human" element={<HumanFeaturesForm />} />
+          <Route path="test/auditory" element={<AuditoryTest />} />
 
-                        <Route path="test/visual" element={<VisualTestLayout />}>
-                            <Route
-                                path="instruction"
-                                element={<TestDispatcher testType="visual" />}
-                            />
-                            {/* Visual Test sử dụng params cho các bước */}
-                            <Route
-                                path=":type/:cardQuantity"
-                                element={<TestDispatcher testType="visual" />}
-                            />
-                            <Route
-                                path="rating"
-                                element={<TestDispatcher testType="visual" />}
-                            />
-                        </Route>
+          <Route path="test/visual" element={<VisualTestLayout />}>
+            <Route
+              path="instruction"
+              element={<TestDispatcher testType="visual" />}
+            />
+            {/* Visual Test sử dụng params cho các bước */}
+            <Route
+              path=":type/:cardQuantity"
+              element={<TestDispatcher testType="visual" />}
+            />
+            <Route
+              path="rating"
+              element={<TestDispatcher testType="visual" />}
+            />
+          </Route>
 
-                        <Route path="test/language" element={<LanguageTestLayout />}>
-                            <Route
-                                path="instruction"
-                                element={<TestDispatcher testType="language" />}
-                            />
-                            {/* Language Test sử dụng params cho các loại câu hỏi */}
-                            <Route
-                                path=":type"
-                                element={<TestDispatcher testType="language" />}
-                            />
-                            <Route
-                                path="rating"
-                                element={<TestDispatcher testType="language" />}
-                            />
-                        </Route>
-                        
-                        {/* 🚀 ROUTE CHO MINIGAME 2 🚀 */}
-                        {/* Sử dụng Minigame2Layout để cung cấp TestStepProvider và steps */}
-                        <Route path="test/minigame2" element={<MiniGame2Layout />}>
-                            <Route
-                                path="instruction"
-                                element={<TestDispatcher testType="minigame2" />}
-                            />
-                            <Route
-                                path="start" // Game chính
-                                element={<TestDispatcher testType="minigame2" />}
-                            />
-                            <Route
-                                path="rating"
-                                element={<TestDispatcher testType="minigame2" />}
-                            />
-                        </Route>
-                        {/* ------------------------------------- */}
+          <Route path="test/language" element={<LanguageTestLayout />}>
+            <Route
+              path="instruction"
+              element={<TestDispatcher testType="language" />}
+            />
+            {/* Language Test sử dụng params cho các loại câu hỏi */}
+            <Route
+              path=":type"
+              element={<TestDispatcher testType="language" />}
+            />
+            <Route
+              path="rating"
+              element={<TestDispatcher testType="language" />}
+            />
+          </Route>
 
-                        {/* 404 */}
-                        <Route path="*" element={<NotFound />} />
-                    </Route>
-                </Routes>
-            </AuthProvider>
-        </Router>
-    );
+          {/* 🚀 ROUTE CHO MINIGAME 2 🚀 */}
+          {/* Sử dụng Minigame2Layout để cung cấp TestStepProvider và steps */}
+          <Route path="test/minigame2" element={<MiniGame2Layout />}>
+            <Route
+              path="instruction"
+              element={<TestDispatcher testType="minigame2" />}
+            />
+            <Route
+              path="start" // Game chính
+              element={<TestDispatcher testType="minigame2" />}
+            />
+            <Route
+              path="rating"
+              element={<TestDispatcher testType="minigame2" />}
+            />
+          </Route>
+          {/* ------------------------------------- */}
+
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -4,20 +4,20 @@ import Footer from "./Footer";
 
 const Layout = () => {
 	return (
-		<div
-			className="
+    <div
+      className="
       min-h-screen 
       flex flex-col 
       bg-gradient-pink
     "
-		>
-			<Header />
-			<main
-				className="
+    >
+      <Header />
+      <main
+        className="
           flex-grow 
-          container mx-auto 
-          p-4 md:p-8 
-          my-4 md:my-8 
+          mx-3 md:mx-8 lg:mx-16 xl:mx-24
+          p-4 md:p-8
+          my-4 md:my-8
           rounded-[2.5rem] 
           bg-white/80 
           shadow-xl
@@ -25,12 +25,12 @@ const Layout = () => {
           min-h-[350px]
           flex flex-col justify-center
         "
-			>
-				<Outlet />
-			</main>
-			<Footer />
-		</div>
-	);
+      >
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
